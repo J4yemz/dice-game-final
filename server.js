826aa64939ae16a01e90820471d4db2aa808e32d
J4yemz <johnmarkeisaias@gmail.com>
2026-03-07 00:15:57 +0800

    ready to deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 
 // Serve static files
 app.use(express.static(path.join(__dirname)));
